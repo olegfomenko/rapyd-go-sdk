a08a6742e33c61d3cd55d3d99935fc6967b3404a
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2021-09-02 20:17:31 +0300

    adding philippines.go

---
+++ payments/philippines.go
@@ -0,0 +1,14 @@
+package payments
+
+import "github.com/olegfomenko/rapyd-go-sdk/resources"
+
+const BancnetPaymentType = "ph_bancnet_bank"
+
+func BancnetBank(description string) *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{
+			"description": description,
+		},
+		Type: BancnetPaymentType,
+	}
+}
