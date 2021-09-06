75dc385e248ee7d6f42079c50a47ce57e62f38e2
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2021-09-06 17:12:51 +0300

    adding vietnam payment types

---
+++ payments/vietnam/main.go
@@ -0,0 +1,270 @@
+package vietnam
+
+import "github.com/olegfomenko/rapyd-go-sdk/resources"
+
+const (
+	VIBPaymentType          = "vn_vib_bank"
+	PGPaymentType           = "vn_pgbank_bank"
+	SaigonPaymentType       = "vn_saigonbank_bank"
+	ACBPaymentType          = "vn_acb_bank"
+	DongaPaymentType        = "vn_dongabank_bank"
+	TechcomPaymentType      = "vn_techcombank_bank"
+	KienlongPaymentType     = "vn_kienlongbank_bank"
+	NCBPaymentType          = "vn_ncb_bank"
+	GPPaymentType           = "vn_gpbank_bank"
+	SHBPaymentType          = "vn_shb_bank"
+	PVComPaymentType        = "vn_pvcombank_bank"
+	VPPaymentType           = "vn_vpbank_bank"
+	VietcomPaymentType      = "vn_vietcombank_bank"
+	NamaPaymentType         = "vn_namabank_bank"
+	MaritimePaymentType     = "vn_maritimebank_bank"
+	SCBPaymentType          = "vn_scbbank_bank"
+	EximPaymentType         = "vn_eximbank_bank"
+	BIDVPaymentType         = "vn_bidv_bank"
+	OceanPaymentType        = "vn_oceanbank_bank"
+	NaviPaymentType         = "vn_navibank_bank"
+	HDPaymentType           = "vn_hdbank_bank"
+	VietaPaymentType        = "vn_vietabank_bank"
+	ABPaymentType           = "vn_abbank_bank"
+	LienVietPostPaymentType = "vn_lienvietpostbank_bank"
+	VRBPaymentType          = "vn_vrb_bank"
+	MBPaymentType           = "vn_mb_bank"
+	TienphongPaymentType    = "vn_tienphongbank_bank"
+	AgriPaymentType         = "vn_agribank_bank"
+	BacaPaymentType         = "vn_bacabank_bank"
+	BVBPaymentType          = "vn_bvb_bank"
+	SeaPaymentType          = "vn_seabank_bank"
+	VietinPaymentType       = "vn_vietinbank_bank"
+	SacomPaymentType        = "vn_sacombank_bank"
+)
+
+func VIBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   VIBPaymentType,
+	}
+}
+
+func PGBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   PGPaymentType,
+	}
+}
+
+func SaigonBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   SaigonPaymentType,
+	}
+}
+
+func ACBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   ACBPaymentType,
+	}
+}
+
+func DongaBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   DongaPaymentType,
+	}
+}
+
+func TechcomBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   TechcomPaymentType,
+	}
+}
+
+func KienlongBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   KienlongPaymentType,
+	}
+}
+
+func NCBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   NCBPaymentType,
+	}
+}
+
+func GPBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   GPPaymentType,
+	}
+}
+
+func SHBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   SHBPaymentType,
+	}
+}
+
+func PVComBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   PVComPaymentType,
+	}
+}
+
+func VPBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   VPPaymentType,
+	}
+}
+
+func VietcomBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   VietcomPaymentType,
+	}
+}
+
+func NamaBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   NamaPaymentType,
+	}
+}
+
+func MaritimeBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   MaritimePaymentType,
+	}
+}
+
+func SCBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   SCBPaymentType,
+	}
+}
+
+func EximBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   EximPaymentType,
+	}
+}
+
+func BIDVBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   BIDVPaymentType,
+	}
+}
+
+func OceanBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   OceanPaymentType,
+	}
+}
+
+func NaviBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   NaviPaymentType,
+	}
+}
+
+func HDBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   HDPaymentType,
+	}
+}
+
+func VietaBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   VietaPaymentType,
+	}
+}
+
+func ABBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   ABPaymentType,
+	}
+}
+
+func LienVietPostBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   LienVietPostPaymentType,
+	}
+}
+
+func VRBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   VRBPaymentType,
+	}
+}
+
+func MBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   MBPaymentType,
+	}
+}
+
+func TienphongBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   TienphongPaymentType,
+	}
+}
+
+func AgriBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   AgriPaymentType,
+	}
+}
+
+func BacaBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   BacaPaymentType,
+	}
+}
+
+func BVBBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   BVBPaymentType,
+	}
+}
+
+func SeaBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   SeaPaymentType,
+	}
+}
+
+func VietinBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   VietinPaymentType,
+	}
+}
+
+func SacomBank() *resources.PaymentMethod {
+	return &resources.PaymentMethod{
+		Fields: map[string]interface{}{},
+		Type:   SacomPaymentType,
+	}
+}
