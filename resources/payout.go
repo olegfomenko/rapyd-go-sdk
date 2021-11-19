d0de8cbce8aa53b55b9eb48dfe77da08e31423c3
Nikita Shaburov <nikita.shaburov1@gmail.com>
2021-11-19 17:46:18 +0200

    add payout entity

---
+++ resources/payout.go
@@ -0,0 +1,25 @@
+package resources
+
+type CreatePayout struct {
+	Beneficiary           Beneficiary `json:"beneficiary"`
+	BeneficiaryCountry    *string     `json:"beneficiary_country,omitempty"`
+	BeneficiaryEntityType string      `json:"beneficiary_entity_type"`
+	ConfirmAutomatically  *bool       `json:"confirm_automatically,omitempty"`
+	Description           *string     `json:"description,omitempty"`
+	Expiration            *string     `json:"expiration,omitempty"`
+	EWallet               *string     `json:"ewallet,omitempty"`
+	PayoutAmount          float64     `json:"payout_amount"`
+	PayoutCurrency        string      `json:"payout_currency"`
+	PayoutMethodType      string      `json:"payout_method_type"`
+	Sender                Sender      `json:"sender"`
+	SenderAmount          *float64    `json:"sender_amount,omitempty"`
+	SenderCountry         string      `json:"sender_country"`
+	SenderCurrency        string      `json:"sender_currency"`
+	SenderEntityType      string      `json:"sender_entity_type"`
+	StatementDescriptor   *string     `json:"statement_descriptor,omitempty"`
+
+}
+
+type CreatePayoutResponse struct {
+	Data Data `json:"data"`
+}
